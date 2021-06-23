b8e10f6e8af59a18aac12d1b6240ecd72c9d535f
wangxiang <wangxiang@tangiplay.com>
2021-06-23 17:06:19 +0800

    modify dependencies; remote clear & test command;

--- index.js
+++ index.js
@@ -11,27 +11,11 @@ const shell = require('shelljs');
 
 yargs(hideBin(process.argv))
   .command(
-    'test',
-    '',
-    (yargs) => yargs,
-    (argv) => {
-      shell.exec(`node_modules/.bin/jest -c ${__dirname}/jest.config.js --rootDir . --color`);
-    }
-  )
-  .command(
     'build',
     '',
     (yargs) => yargs,
     (argv) => {
       shell.exec(`node_modules/.bin/gulp -f ${__dirname}/gulpfile.js --cwd .  --color build`);
-    }
-  )
-  .command(
-    'clear',
-    '',
-    (yargs) => yargs,
-    (argv) => {
-      shell.exec(`node_modules/.bin/rimraf dist`);
     }
   )
   .command(
